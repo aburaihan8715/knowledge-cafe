25e59eafdf9795f47b7bb17190520fd3d8ea05a7
aburahan8721 <aburaihan8721@gmail.com>
2023-05-02 18:57:24 +0600

    ui done

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -2,33 +2,155 @@ import React from "react";
 import "./Blogs.css";
 import blogImg from "../../assets/tech.jpg";
 import authorImg from "../../assets/user.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const Blogs = () => {
   return (
-    <ul className="blogs">
-      <li>
-        <article>
-          <div>
-            <img src={blogImg} alt="blog photo" />
+    <ul className="blogs_list">
+      {/* blogs start */}
+      <li className="blogs_item">
+        <article className="blogs_content">
+          <div className="blogs_cover_photo_box">
+            <img className="blogs_cover_photo" src={blogImg} alt="blog photo" />
           </div>
 
-          <div>
-            <div>
-              <img src={authorImg} alt="author photo" />
+          <div className="author_info_box">
+            <div className="author_img_box">
+              <img className="author_img" src={authorImg} alt="author photo" />
             </div>
-            <div>
-              <p>Mr. Raju</p>
-              <p>Mar 14 (4 Days ago)</p>
-            </div>
-            <p>
-              <span>10 min</span>read <span>
-                
-              </span>
 
+            <div className="author_name_box">
+              <p className="author_name heading_secondary">Mr. Raju</p>
+              <p className="publish_date">Mar 14 (4 Days ago)</p>
+            </div>
+
+            <p className="read_time_box">
+              <span className="read_time">10 min </span>read{" "}
+              <button className="bookmark_btn">
+                <FontAwesomeIcon icon={faBookmark} />
+              </button>
             </p>
           </div>
+
+          <h2 className="heading_primary">How to get your first job as a self-taught programmer</h2>
+
+          <p className="blogs_type">#programming</p>
+
+          <a className="mark_as_read_link" href="#">
+            Mark as read
+          </a>
         </article>
       </li>
+      {/* blogs start */}
+
+      {/* blogs start */}
+      <li className="blogs_item">
+        <article className="blogs_content">
+          <div className="blogs_cover_photo_box">
+            <img className="blogs_cover_photo" src={blogImg} alt="blog photo" />
+          </div>
+
+          <div className="author_info_box">
+            <div className="author_img_box">
+              <img className="author_img" src={authorImg} alt="author photo" />
+            </div>
+
+            <div className="author_name_box">
+              <p className="author_name heading_secondary">Mr. Raju</p>
+              <p className="publish_date">Mar 14 (4 Days ago)</p>
+            </div>
+
+            <p className="read_time_box">
+              <span className="read_time">10 min </span>read{" "}
+              <button className="bookmark_btn">
+                <FontAwesomeIcon icon={faBookmark} />
+              </button>
+            </p>
+          </div>
+
+          <h2 className="heading_primary">How to get your first job as a self-taught programmer</h2>
+
+          <p className="blogs_type">#programming</p>
+
+          <a className="mark_as_read_link" href="#">
+            Mark as read
+          </a>
+        </article>
+      </li>
+      {/* blogs start */}
+
+      {/* blogs start */}
+      <li className="blogs_item">
+        <article className="blogs_content">
+          <div className="blogs_cover_photo_box">
+            <img className="blogs_cover_photo" src={blogImg} alt="blog photo" />
+          </div>
+
+          <div className="author_info_box">
+            <div className="author_img_box">
+              <img className="author_img" src={authorImg} alt="author photo" />
+            </div>
+
+            <div className="author_name_box">
+              <p className="author_name heading_secondary">Mr. Raju</p>
+              <p className="publish_date">Mar 14 (4 Days ago)</p>
+            </div>
+
+            <p className="read_time_box">
+              <span className="read_time">10 min </span>read{" "}
+              <button className="bookmark_btn">
+                <FontAwesomeIcon icon={faBookmark} />
+              </button>
+            </p>
+          </div>
+
+          <h2 className="heading_primary">How to get your first job as a self-taught programmer</h2>
+
+          <p className="blogs_type">#programming</p>
+
+          <a className="mark_as_read_link" href="#">
+            Mark as read
+          </a>
+        </article>
+      </li>
+      {/* blogs start */}
+
+      {/* blogs start */}
+      <li className="blogs_item">
+        <article className="blogs_content">
+          <div className="blogs_cover_photo_box">
+            <img className="blogs_cover_photo" src={blogImg} alt="blog photo" />
+          </div>
+
+          <div className="author_info_box">
+            <div className="author_img_box">
+              <img className="author_img" src={authorImg} alt="author photo" />
+            </div>
+
+            <div className="author_name_box">
+              <p className="author_name heading_secondary">Mr. Raju</p>
+              <p className="publish_date">Mar 14 (4 Days ago)</p>
+            </div>
+
+            <p className="read_time_box">
+              <span className="read_time">10 min </span>read{" "}
+              <button className="bookmark_btn">
+                <FontAwesomeIcon icon={faBookmark} />
+              </button>
+            </p>
+          </div>
+
+          <h2 className="heading_primary">How to get your first job as a self-taught programmer</h2>
+
+          <p className="blogs_type">#programming</p>
+
+          <a className="mark_as_read_link" href="#">
+            Mark as read
+          </a>
+        </article>
+      </li>
+      {/* blogs start */}
     </ul>
   );
 };
