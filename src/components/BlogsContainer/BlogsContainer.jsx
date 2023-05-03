b87f0ef0f9b314c7adda98e7ac138c796ef93969
aburahan8721 <aburaihan8721@gmail.com>
2023-05-03 09:22:01 +0600

    api render done

--- src/components/BlogsContainer/BlogsContainer.jsx
+++ src/components/BlogsContainer/BlogsContainer.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./BlogsContainer.css"
 import Blogs from '../Blogs/Blogs';
 import Sidebar from '../Sidebar/Sidebar';
 
 const BlogsContainer = () => {
+
+  const [blogs,setBlogs]=useState([])
+
+  useEffect(()=>{
+    fetch("data.json")
+    .then(res=>res.json())
+    .then(data=>setBlogs(data))
+  },[])
+
+
   return (
     <div className='blogs_container'>
-      <Blogs></Blogs>
+      <Blogs blogs={blogs}></Blogs>
       <Sidebar></Sidebar>
     </div>
   );
